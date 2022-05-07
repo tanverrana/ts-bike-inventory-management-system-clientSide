61ea2e844c6075bf259b8caaccf1732a073a2a05
tanverrana <tanversobur@gmail.com>
2022-05-08 01:12:56 +0600

    fake data added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/blog" element={<Blogs></Blogs>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
