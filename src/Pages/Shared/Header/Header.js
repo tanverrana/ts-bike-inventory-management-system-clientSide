e5f711b0e59ebe8615b06ff5baab867fb22bb31b
tanverrana <tanversobur@gmail.com>
2022-05-08 15:44:44 +0600

    style link button

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
                             <Nav.Link as={Link} to="about">About</Nav.Link>
                             {
                                 user ?
-                                    <button onClick={handleSignOut}>SignOut</button>
+                                    <button className="btn btn-link text-white text-decoration-none" onClick={handleSignOut}>SignOut</button>
                                     :
                                     <Nav.Link as={Link} to="login">Login </Nav.Link>
                             }
