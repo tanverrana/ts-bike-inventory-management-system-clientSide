ef17cf6685b0bd44f8002d622eb4df090444b790
tanverrana <tanversobur@gmail.com>
2022-05-09 00:54:18 +0600

    get data from client side

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -8,7 +8,7 @@ const Products = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch("products.json")
+        fetch("http://localhost:5000/inventory")
             .then(res => res.json())
             .then(data => setProducts(data));
     }, [])
