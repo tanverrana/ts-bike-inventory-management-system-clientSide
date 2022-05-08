42df4b79dee3c6589da4f7072fb3c9fcd7981fcf
tanverrana <tanversobur@gmail.com>
2022-05-08 13:42:06 +0600

    create login and register components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
 import NotFound from './Pages/NotFound/NotFound';
 import Inventory from './Pages/Inventory/Inventory';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/inventory/:inventoryId" element={<Inventory></Inventory>}></Route>
         <Route path="/blog" element={<Blogs></Blogs>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
         <Route path="/*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Inventory = () => {
+    const { inventoryId } = useParams();
     return (
         <div>
-            <h2>This is inventory page.</h2>
+            <h2>This is inventory page:{inventoryId}</h2>
         </div>
     );
 };
