ee6a4f48cdd0661850c6a2cc05b0314eb12b6863
tanverrana <tanversobur@gmail.com>
2022-05-08 14:55:26 +0600

    create user

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,20 +1,32 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import "./Register.css";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
     const navigate = useNavigate();
 
     const navigateLogin = () => {
         navigate("/login");
+    }
+
+    if (user) {
+        navigate("/");
     }
     const handleRegister = event => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-
         createUserWithEmailAndPassword(email, password);
     }
     return (
