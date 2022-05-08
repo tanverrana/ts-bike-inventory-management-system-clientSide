000337c333f2d2f0d75415f0c5a043cec7ce55c1
tanverrana <tanversobur@gmail.com>
2022-05-09 01:21:36 +0600

    load single data

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Inventory = () => {
     const { inventoryId } = useParams();
+    const [inventory, setInventory] = useState({});
+
+    useEffect(() => {
+        const url = `http://localhost:5000/inventory/${inventoryId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setInventory(data));
+    }, [])
     return (
-        <div>
-            <h2>This is inventory page:{inventoryId}</h2>
+        <div className="container">
+            <h2>Selected Inventory Id:{inventoryId}</h2>
+            <div className="product-container">
+                <img height="300px" width="300px" src={inventory.img} alt="" />
+                <h2>{inventory.name}</h2>
+                <h5>Price:${inventory.price}</h5>
+                <p>Quantity:{inventory.quantity}</p>
+                <p>{inventory.details}</p>
+                <p><small>Supplier:{inventory.supplier}</small></p>
+            </div>
+            <div className="text-center">
+                <button className="btn btn-primary m-4">Deliver</button> <button className="btn btn-primary">Add</button>
+            </div>
         </div>
     );
 };
